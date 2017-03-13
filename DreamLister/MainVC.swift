89f05e08f98ea77d6d183d010864f01aad7d216c
Sean Perez <sean.perez29@gmail.com>
2017-03-12 18:53:50 -0600

    Setup NSFetchedResultsControllerDelegate

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -55,5 +55,37 @@ extension MainVC: UITableViewDelegate, UITableViewDataSource {
 
 extension MainVC: NSFetchedResultsControllerDelegate {
     
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        guard let newIndexPath = newIndexPath else {
+            print("Unable to obtain a newIndexPath")
+            return
+        }
+        guard let indexPath = indexPath else {
+            print("Unable to obtain an indexPath")
+            return
+        }
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [newIndexPath], with: .fade)
+        case .update:
+            if let cell = tableView.cellForRow(at: indexPath) as? ItemCell {
+                
+            }
+        case .move:
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.insertRows(at: [newIndexPath], with: .fade)
+        }
+    }
+    
 }
 
